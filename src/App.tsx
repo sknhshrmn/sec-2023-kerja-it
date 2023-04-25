255f6b883d6bf1b6e9bd782d7c4973e05210027b
sknhshrmn <sakinahshahriman@gmail.com>
2023-04-26 04:18:34 +0800

    add: test the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import {
   // Route,
 } from "react-router-dom";
 import Home from "./pages/Home";
-import Job from "./pages/Job";
 // import ScrollToTop from "./components/ScrollToTop";
 // import { BASEURL } from "./constant/config";
 
