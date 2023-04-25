9753bd376b38c86d85cba1aab00ff2471136f691
sknhshrmn <sakinahshahriman@gmail.com>
2023-04-26 03:58:04 +0800

    add: test the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import {
-  // RouterProvider,
+  RouterProvider,
   createBrowserRouter,
   // BrowserRouter,
   // Routes,
@@ -14,7 +11,6 @@ import Job from "./pages/Job";
 // import { BASEURL } from "./constant/config";
 
 function App() {
-  const [count, setCount] = useState(0);
   const router = createBrowserRouter([
     {
       path: "/sec-2023-react-js-kerja-it",
@@ -25,32 +21,8 @@ function App() {
       element: <Job />,
     },
   ]);
-  console.log(router);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
